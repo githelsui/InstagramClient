060b237b71c90cf239d9855bae3e4f9b95edfa1a
Githel Lynn Suico <githelsuico@fb.com>
2020-07-06 16:35:40 -0700

    Setup user story 5: integrate UIImagePicker and Camera

---
+++ InstagramClient/ComposeViewController.h
@@ -0,0 +1,17 @@
+//
+//  ComposeViewController.h
+//  InstagramClient
+//
+//  Created by Githel Lynn Suico on 7/6/20.
+//  Copyright © 2020 Githel Lynn Suico. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
