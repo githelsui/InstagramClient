888da0d2cb276b60e262ef36318eedb0c2a84f79
Githel Lynn Suico <githelsuico@fb.com>
2020-07-06 14:29:46 -0700

    upload parse files

---
+++ Pods/Target Support Files/Pods-InstagramClient-InstagramClientUITests/Pods-InstagramClient-InstagramClientUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_InstagramClient_InstagramClientUITests : NSObject
+@end
+@implementation PodsDummy_Pods_InstagramClient_InstagramClientUITests
+@end
